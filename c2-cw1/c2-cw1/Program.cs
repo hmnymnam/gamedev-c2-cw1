df6b2abce18ab4128717e0e38318a4f1f329803b
hmnymnam <100356782+hmnymnam@users.noreply.github.com>
2022-03-05 11:46:34 +0300

    classwork 1 L2

---
+++ c2-cw1/c2-cw1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "hero";
+            double characterSpeed = 3.3;
+            int characterAge = 30;
+            string superPower1 = "night vision";
+
+            string superPower2 = "self healing";
+                Console.WriteLine("my name is {0}, my super powers are {1} and {2} , my age is {3} and my speed is {4}" ,characterName, superPower1,superPower2, characterAge,characterSpeed);
+        }
+    }
+}
